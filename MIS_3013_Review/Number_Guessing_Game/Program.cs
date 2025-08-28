72c095d5f3b8400c4d4f994dfb8b1dfd35a19847
ErnestoMoralesPinales <164249438+ErnestoMoralesPinales@users.noreply.github.com>
2025-08-28 13:01:57 -0500

    halfway through

--- MIS_3013_Review/Number_Guessing_Game/Program.cs
+++ MIS_3013_Review/Number_Guessing_Game/Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+// data_type variable_name = value
+Random r = new Random();
+
+var rando = r.Next(1, 6); //Generate a random number between 1-5
+Console.WriteLine($"Generated a random number with the value: {rando}");
+
+int guess;
+
+int count = 0;
+
+do
+{
+	Console.WriteLine("Please guess a number between 1-5 << ");
+	string user_guess = Console.ReadLine();
+
+	 guess = int.Parse(user_guess);
+    count++;
+
+    Console.WriteLine("You guessed " + user_guess);
+
+	if (guess != rando)
+	{
+		if (guess > rando)
+		{
+			Console.WriteLine("Sorry, you guessed too high");
+
+		}
+		else
+		{
+            Console.WriteLine("Sorry, you guessed low");
+        }
+    }
+
+} while (guess != rando);
+
+Console.WriteLine("Congratulations, you guessed correctly!");
