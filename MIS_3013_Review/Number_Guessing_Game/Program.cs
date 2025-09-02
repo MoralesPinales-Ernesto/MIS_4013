51fc83047c72dfc6e24c12ad3a9f88a46fbba0e7
ErnestoMoralesPinales <164249438+ErnestoMoralesPinales@users.noreply.github.com>
2025-09-02 12:29:56 -0500

    Completed step 4

--- MIS_3013_Review/Number_Guessing_Game/Program.cs
+++ MIS_3013_Review/Number_Guessing_Game/Program.cs
@@ -2,40 +2,68 @@
 // data_type variable_name = value
 Random r = new Random();
 
-var rando = r.Next(1, 6); //Generate a random number between 1-5
-Console.WriteLine($"Generated a random number with the value: {rando}");
+	//input upper and lower bounds
+	Console.WriteLine("Please input lower bound value <<");
+	string answer = Console.ReadLine();
+	int lowerbound = int.Parse(answer);
 
-int guess;
+	Console.WriteLine("Please input upper bound value <<");
+	answer = Console.ReadLine();
+	int upperbound = int.Parse(answer);
 
-int count = 0;
-
-do
+for (int i = 0; i < int.MaxValue; i++)
 {
-	Console.WriteLine("Please guess a number between 1-5 << ");
-	string user_guess = Console.ReadLine();
+	var rando = r.Next(lowerbound, upperbound); //Generate a random number between lowerbound and upperbound
+	Console.WriteLine($"Generated a random number with the value: {rando}");
 
-	 guess = int.Parse(user_guess);
-    count++;
+	//initialize guess
+	int guess;
+	//initialize and declare count
+	int count = 0;
 
-    Console.WriteLine("You guessed " + user_guess);
 
-	if (guess != rando)
+
+	do
 	{
-		if (guess > rando)
-		{
-			Console.ForegroundColor = ConsoleColor.Red;
-			Console.WriteLine("Sorry, you guessed too high");
-			Console.ForegroundColor = ConsoleColor.White;
+		Console.WriteLine("Please guess a number between " + lowerbound + " and " + upperbound + " << ");
+		string user_guess = Console.ReadLine();
 
+		guess = int.Parse(user_guess);
+		count++;
+
+		Console.WriteLine("You guessed " + user_guess);
+
+		if (guess != rando)
+		{
+			if (guess > rando)
+			{
+				Console.ForegroundColor = ConsoleColor.Red;
+				Console.WriteLine("Sorry, you guessed too high");
+				Console.ForegroundColor = ConsoleColor.White;
+
+			}
+			else
+			{
+				Console.ForegroundColor = ConsoleColor.Blue;
+				Console.WriteLine("Sorry, you guessed low");
+				Console.ForegroundColor = ConsoleColor.White;
+			}
 		}
-		else
-		{
-			Console.ForegroundColor = ConsoleColor.Blue;
-            Console.WriteLine("Sorry, you guessed low");
-			Console.ForegroundColor = ConsoleColor.White;
-        }
-    }
 
-} while (guess != rando);
+	} while (guess != rando);
 
-Console.WriteLine("Congratulations, you guessed correctly and it only took " + count + " attempts!");
+	Console.WriteLine("Congratulations, you guessed correctly and it only took " + count + " attempts!");
+
+    Console.WriteLine("Do you want to try your luck again? yes or no <<");
+
+	answer = Console.ReadLine();
+
+	if (answer != "yes")
+	{
+		break;
+		//i = int.maxvalue;
+		//return;
+	}
+}
+
+Console.WriteLine("Thank you for playing our game, come back soon!");
